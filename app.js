947a1aff426b0e0f682cf35b01af397c5c2783b5
Jayakrushna35 <jayakrushnapattnaik6@gmail.com>
2025-02-27 11:51:46 +0530

    authentaction using passport start

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const connectDB = require("./server/config/db");
+const session = require("express-session");
+const passport = require("passport");
+const Mongostor = require("connect-mongo")
 
 
 const app = express();
 const port = 8000 || process.env.PORT;
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+connectDB();
 
 // Static files
 app.use(express.static("public"));
@@ -22,6 +30,7 @@ app.set("view engine", "ejs");
 
 
 //Routes
+app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
