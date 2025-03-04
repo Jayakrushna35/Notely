77f0e89709d2ab057c69dcb4f10e6cbbb3365ad8
Jayakrushna35 <jayakrushnapattnaik6@gmail.com>
2025-03-04 16:49:39 +0530

    show username and create new notes

--- server/controller/dashboardcontroller.js
+++ server/controller/dashboardcontroller.js
@@ -1,13 +1,13 @@
-/**
- * GET/
- * Homepage
- */
+const Notes = require("../models/Notes");
+const mongoose = require('mongoose');
+ 
 exports.dashboard = async (req,res) =>{
     const locals = {
         title:"Dashboard",
         description: "free NodeJs Notes App",
     }
     res.render('dashboard/index', {
+        userName: req.user.firstName,
         locals,
         layout: '../views/layouts/dashboard'
     });  
